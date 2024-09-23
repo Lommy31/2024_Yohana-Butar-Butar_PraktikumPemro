cb349fa5328dc3d135285d71c3d476988b1ea7e8
Yohana-mahasiswa <yohanabutarbutar546690@mail.ugm.ac.id>
2024-09-23 22:32:13 +0700

    Update Sum_Natural_Numbers.cpp

--- Week4/problem_1/Sum_Natural_Numbers.cpp
+++ Week4/problem_1/Sum_Natural_Numbers.cpp
@@ -1,1 +1,26 @@
 #include <iostream>
+using namespace std;
+
+int main() {
+    int n, sum = 0;
+
+    cout << "Input: "; //asking the user by enter the value 
+    cin >> n;
+
+    for (int i = 1; i <= n; ++i) { //to calculate the sum by loop
+        sum += i;
+    }
+
+    cout << "Output: Sum = " << sum << endl;//the sum of the input
+
+    cout << "Explanation: ";
+    for (int i = 1; i <= n; ++i) {
+        cout << i;
+        if (i < n) {
+            cout << " + ";
+        }
+    }
+    cout << " = " << sum << endl;
+
+    return 0;
+}
