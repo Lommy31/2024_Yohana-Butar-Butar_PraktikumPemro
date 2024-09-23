405b5772685c2e1085a084e214226b74fca15c34
Yohana-mahasiswa <yohanabutarbutar546690@mail.ugm.ac.id>
2024-09-23 22:32:49 +0700

    Update Multiplicatin_Numbers.cpp

--- Week4/problem_2/Multiplicatin_Numbers.cpp
+++ Week4/problem_2/Multiplicatin_Numbers.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Input: "; //asking the user to enter the value
+    cin >> n;
+
+    cout << "Output:" << endl; //show the output of the multiplication 
+
+    for (int i = 1; i <= 3; ++i) { 
+        cout << n << " x " << i << " = " << n * i << endl;
+    } //Showing the first three rows of the multiplication table
+    
+    cout << "..." << endl;
+
+    cout << n << " x 10 = " << n * 10 << endl; //showing the last row from the multiplication table
+
+    return 0;
+}
+
 
