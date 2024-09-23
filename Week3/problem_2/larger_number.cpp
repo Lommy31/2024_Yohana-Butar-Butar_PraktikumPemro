6f8a5ae12418ba7f1ef74f0667c45c0017da096f
Yohana-mahasiswa <yohanabutarbutar546690@mail.ugm.ac.id>
2024-09-23 15:18:20 +0700

    Update and rename larger number.cpp to larger_number.cpp

---
+++ Week3/problem_2/larger_number.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int number1, number2, number3;
+cout << "Enter the first number: ";
+cin >> number1;
+cout << "Enter the second number: ";
+cin >> number2;
+cout << "Enter the third number: ";
+cin >> number3;
+if (number1 >= number2) {
+if (number1 >= number3) {
+cout << "\nThe large number is: " << number1 << endl;
+} else {
+cout << "\nThe large number is: " << number3 << endl;
+}
+} else {
+if (number2 >= number3) {
+cout << "\nThe large number is: " << number2 << endl;
+} else {
+cout << "\nThe large number is: " << number3 << endl;
+}
+}
+return 0;
+}
