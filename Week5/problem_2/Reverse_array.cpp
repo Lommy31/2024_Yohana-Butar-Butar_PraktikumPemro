f7d2856b12bb57d7d2ed6eb0a40f667195fd4431
Yohana-mahasiswa <yohanabutarbutar546690@mail.ugm.ac.id>
2024-09-27 15:09:54 +0700

    Update Reverse_array.cpp

--- Week5/problem_2/Reverse_array.cpp
+++ Week5/problem_2/Reverse_array.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm> 
+using namespace std;
+
+int main() {
+    
+    vector<int> numbers = {1, 2, 3, 4, 5}; //the value of the input
+
+    cout << "Input: [";
+    for (size_t i = 0; i < numbers.size(); ++i) {
+        std::cout << numbers[i] << (i != numbers.size() - 1 ? ", " : "");
+    }
+    cout << "]" << std::endl;
+
+    vector<int> reversedNumbers = numbers;  // Make a copy of the input vector
+    reverse(reversedNumbers.begin(), reversedNumbers.end());
+
+    cout << "Output: [";
+    for (size_t i = 0; i < reversedNumbers.size(); ++i) {
+        cout << reversedNumbers[i] << (i != reversedNumbers.size() - 1 ? ", " : "");
+    }
+    cout << "]" << std::endl;
+
+    return 0;
+}
 
