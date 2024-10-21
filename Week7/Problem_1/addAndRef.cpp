8bb30a51f4be857a7c4b194d0553492e2824ad55
Yohana-mahasiswa <yohanabutarbutar546690@mail.ugm.ac.id>
2024-10-21 13:58:09 +0700

    Update addAndRef.cpp

--- Week7/Problem_1/addAndRef.cpp
+++ Week7/Problem_1/addAndRef.cpp
@@ -1,1 +1,27 @@
 
+#include <iostream>
+using namespace std;
+
+  void addition(int x, int y){
+        int add = x + y; //fincton to perform adddition
+        cout << "the sum of " << x << " and " << y << " is: " << add << endl;
+  }
+  
+  void difference(int x, int y){
+        int add = x - y; //fincton to perform adddition
+        cout << "the sum of " << x << " and " << y << " is: " << add << endl;
+  }
+
+int main() { //main function
+
+    int x,y;
+    cout << "Exercise Using Subprogram and Function One\n";
+    cout << "==========================================\n";
+    cout << "The Value of x: "; //imput the value of x
+    cin >> x >> y;
+    
+    addition (x,y); //calling the function
+    difference (x,y);
+    
+    return 0;
+}
