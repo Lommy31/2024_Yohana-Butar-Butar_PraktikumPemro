f9719664bf57f7fe9ddca382545cca6c9ba5fb9c
Yohana-mahasiswa <yohanabutarbutar546690@mail.ugm.ac.id>
2024-10-21 15:44:33 +0700

    Update factorial.cpp

--- Week7/Problem_2/factorial.cpp
+++ Week7/Problem_2/factorial.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+using namespace std;
 
+int finctional (int x);
+
+int functional(int x) { //call itself
+    if (x > 1) {
+        return x * functional(x -1);
+    } else {
+        return 1;
+    }    
+}
+
+int main() {
+    
+    int x;
+    cout << "Exercise Two Recurrence Function to Calculate x!\n";
+    cout << "================================================\n";
+    cout << "Enter The Value of x: ";
+    cin >> x;
+    cout << "The Function of " << x << "! = " << functional(x) << endl;
+
+    return 0;
+}
