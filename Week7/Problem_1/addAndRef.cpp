75d23c470e5f04bdfd43df3c80ccf160b1f72316
Yohana-mahasiswa <yohanabutarbutar546690@mail.ugm.ac.id>
2024-10-21 15:43:57 +0700

    Update addAndRef.cpp

--- Week7/Problem_1/addAndRef.cpp
+++ Week7/Problem_1/addAndRef.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
   void addition(int x, int y){
         int add = x + y; //fincton to perform adddition
-        cout << "the sum of " << x << " and " << y << " is: " << add << endl;
+        cout << "The Addition of " << x << " and " << y << " are: " << add << endl;
   }
   
   void difference(int x, int y){
-        int add = x - y; //fincton to perform adddition
-        cout << "the sum of " << x << " and " << y << " is: " << add << endl;
+        int diff = x - y; //fincton to perform adddition
+        cout << "The Difference of " << x << " and " << y << " are: " << diff << endl;
   }
 
 int main() { //main function
@@ -16,7 +16,7 @@ int main() { //main function
     int x,y;
     cout << "Exercise One Using Subprogram and Function\n";
     cout << "==========================================\n";
-    cout << "The Value of x: "; //imput the value of x
+    cout << "The Value of x and y: "; //imput the value of x
     cin >> x >> y;
     
     addition (x,y); //calling the function
