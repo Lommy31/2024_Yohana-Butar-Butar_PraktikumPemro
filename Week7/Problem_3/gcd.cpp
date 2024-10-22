8cbdf2840b86ce21bca0c1c38d7773c659b8f959
Yohana-mahasiswa <yohanabutarbutar546690@mail.ugm.ac.id>
2024-10-22 12:07:49 +0700

    Update gcd.cpp

--- Week7/Problem_3/gcd.cpp
+++ Week7/Problem_3/gcd.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int gcd(int x, int y) { //finctoin of calculate the value GCD
+int gcd(int x, int y) { //functoin of calculate the value GCD
     if ( x%y == 0) return y;
     else return gcd(y, x%y);
 }
@@ -12,7 +12,7 @@ int main() {
     
     cout << "Exercise Three Recurrence Function to calculate GCD\n";
     cout << "===================================================\n";
-    cout << " The Number of x and y: ";
+    cout << " The Number of x and y: "; // to put the value
     cin >> x >> y;
     cout << "The Value of " << x << " and " << y << " GCD are: ";
     cout << gcd(x,y) << endl; //The total value of GCD
